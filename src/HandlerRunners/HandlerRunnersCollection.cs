f90849a32b289b82bea1ede23a220dc4896aad05
kolan72 <kolan72@gmail.com>
2023-05-12 10:47:39 +0300

    fix formatting

--- src/HandlerRunners/HandlerRunnersCollection.cs
+++ src/HandlerRunners/HandlerRunnersCollection.cs
@@ -6,30 +6,30 @@ using static PoliNorError.HandleErrorPolicyBase;
 namespace PoliNorError
 {
 	public class HandlerRunnersCollection
-    {
-        private List<IEnumerable<IHandlerRunner>> _handlerRunnerBases;
+	{
+		private List<IEnumerable<IHandlerRunner>> _handlerRunnerBases;
 
-        private static readonly Func<IEnumerable<IHandlerRunner>, bool> _func = (ie) => ie.Any();
+		private static readonly Func<IEnumerable<IHandlerRunner>, bool> _func = (ie) => ie.Any();
 
-        public static HandlerRunnersCollection FromSyncAndNotSync(IEnumerable<IHandlerRunner> syncCollection, IEnumerable<IHandlerRunner> asyncCollection)
-        {
-            return new HandlerRunnersCollection() { _handlerRunnerBases = new List<IEnumerable<IHandlerRunner>>() { syncCollection, asyncCollection } };
-        }
+		public static HandlerRunnersCollection FromSyncAndNotSync(IEnumerable<IHandlerRunner> syncCollection, IEnumerable<IHandlerRunner> asyncCollection)
+		{
+			return new HandlerRunnersCollection() { _handlerRunnerBases = new List<IEnumerable<IHandlerRunner>>() { syncCollection, asyncCollection } };
+		}
 
-        public HandlerRunnerSyncType MapToSyncType()
-        {
-            var result = _handlerRunnerBases.CheckCollectionsForCondition(_func);
-            switch (result.ConditionMet)
-            {
-                case EnumerableExtension.ConditionCheckResult.ConditionMetType.NoOne:
-                    return HandlerRunnerSyncType.None;
-                case EnumerableExtension.ConditionCheckResult.ConditionMetType.OnlyOne:
-                    return result.Indx == 0 ? HandlerRunnerSyncType.Sync : HandlerRunnerSyncType.Async;
-                case EnumerableExtension.ConditionCheckResult.ConditionMetType.MoreThenOne:
-                    return HandlerRunnerSyncType.Misc;
-                default:
-                    throw new NotImplementedException();
-            }
-        }
-    }
+		public HandlerRunnerSyncType MapToSyncType()
+		{
+			var result = _handlerRunnerBases.CheckCollectionsForCondition(_func);
+			switch (result.ConditionMet)
+			{
+				case EnumerableExtension.ConditionCheckResult.ConditionMetType.NoOne:
+					return HandlerRunnerSyncType.None;
+				case EnumerableExtension.ConditionCheckResult.ConditionMetType.OnlyOne:
+					return result.Indx == 0 ? HandlerRunnerSyncType.Sync : HandlerRunnerSyncType.Async;
+				case EnumerableExtension.ConditionCheckResult.ConditionMetType.MoreThenOne:
+					return HandlerRunnerSyncType.Misc;
+				default:
+					throw new NotImplementedException();
+			}
+		}
+	}
 }
