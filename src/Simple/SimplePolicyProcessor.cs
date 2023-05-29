0e4d11bc59b93a325cbadde59f3b193993f5b98e
kolan72 <kolan72@gmail.com>
2023-05-29 17:46:29 +0300

    Add `SimplePolicyProcessor.Execute<T>` method and tests on it.

--- src/Simple/SimplePolicyProcessor.cs
+++ src/Simple/SimplePolicyProcessor.cs
@@ -36,6 +36,36 @@ namespace PoliNorError
 			return result;
 		}
 
-		
+		public PolicyResult<T> Execute<T>(Func<T> func, CancellationToken token = default)
+		{
+			var result = PolicyResult<T>.ForSync();
+
+			if (token.IsCancellationRequested)
+			{
+				result.SetCanceled();
+				return result;
+			}
+
+			try
+			{
+				var resAction = func();
+				result.SetOk();
+				result.SetResult(resAction);
+			}
+			catch (OperationCanceledException oe) when (oe.CancellationToken.Equals(token))
+			{
+				result.SetFailedAndCanceled();
+			}
+			catch (AggregateException ae) when (ae.HasCanceledException(token))
+			{
+				result.SetFailedAndCanceled();
+			}
+			catch (Exception ex)
+			{
+				result.AddError(ex);
+				HandleCatchBlockAndChangeResult(ex, result, CatchBlockProcessErrorInfo.FromSimple(), token);
+			}
+			return result;
+		}
 	}
 }
--- tests/SimplePolicyProcessorTests.cs
+++ tests/SimplePolicyProcessorTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace PoliNorError.Tests
 {
@@ -11,16 +12,53 @@ namespace PoliNorError.Tests
 		[TestCase(false, 0, 1)]
 		public void Should_CatchBlockError_Handled_For_Handle(bool throwError, int CatchBlockErrorsCount, int k)
 		{
-			void saveAsync() => throw new Exception();
+			void save() => throw new Exception();
 
 			int i = 0;
 			void errorProcessorFunc(Exception ex) { if (throwError) throw ex; else ++i; } 
 			var retryPolTest = new SimplePolicyProcessor().WithErrorProcessorOf(errorProcessorFunc);
-			var res = retryPolTest.Execute(saveAsync);
+			var res = retryPolTest.Execute(save);
 			Assert.IsTrue(res.Errors.Count() == 1);
 
 			Assert.AreEqual(CatchBlockErrorsCount, res.CatchBlockErrors.Count());
 			Assert.AreEqual(k, i);
+		}
+
+		[Test]
+		public void Should_Handle_If_NoError_Work()
+		{
+			void save() => Expression.Empty();
+			var retryPolTest = new SimplePolicyProcessor();
+			var res = retryPolTest.Execute(save);
+			Assert.IsTrue(res.IsOk);
+		}
+
+		[Test]
+		[TestCase(true, 1, 0)]
+		[TestCase(false, 0, 1)]
+		public void Should_CatchBlockError_Handled_For_HandleT(bool throwError, int CatchBlockErrorsCount, int k)
+		{
+			int save() => throw new Exception();
+
+			int i = 0;
+			void errorProcessorFunc(Exception ex) { if (throwError) throw ex; else ++i; }
+			var retryPolTest = new SimplePolicyProcessor().WithErrorProcessorOf(errorProcessorFunc);
+
+			var res = retryPolTest.Execute(save);
+			Assert.IsTrue(res.Errors.Count() == 1);
+
+			Assert.AreEqual(CatchBlockErrorsCount, res.CatchBlockErrors.Count());
+			Assert.AreEqual(k, i);
+		}
+
+		[Test]
+		public void Should_HandleT_If_NoError_Work()
+		{
+			int save() => 1;
+			var retryPolTest = new SimplePolicyProcessor();
+			var res = retryPolTest.Execute(save);
+			Assert.IsTrue(res.IsOk);
+			Assert.AreEqual(1, res.Result);
 		}
 	}
 }
