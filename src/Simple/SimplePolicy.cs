34db05edce1ab892e4f7f8448e434cb90498f151
kolan72 <kolan72@gmail.com>
2023-06-03 22:03:50 +0300

    Add `SimplePolicy.Handle` method and tests.

---
+++ src/Simple/SimplePolicy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError
+{
+	internal sealed class SimplePolicy : HandleErrorPolicyBase, IPolicyBase
+	{
+		private readonly ISimplePolicyProcessor _simpleProcessor;
+
+		public SimplePolicy(IBulkErrorProcessor processor = null) : this(new SimplePolicyProcessor(processor ?? new BulkErrorProcessor())) { }
+
+		public SimplePolicy(ISimplePolicyProcessor processor) : base(processor) => _simpleProcessor = processor;
+
+		public PolicyResult Handle(Action action, CancellationToken token = default)
+		{
+			if (action == null)
+				return PolicyResult.ForSync().SetFailedWithError(new NoDelegateException(this));
+
+			PolicyResult retryResult = null;
+
+			if (_wrappedPolicy == null)
+			{
+				retryResult = _simpleProcessor.Execute(action, token);
+			}
+			else
+			{
+				var wrapper = new PolicyWrapper(_wrappedPolicy, action, token);
+				Action actionWrapped = wrapper.Handle;
+
+				retryResult = _simpleProcessor.Execute(actionWrapped, token);
+				retryResult.WrappedPolicyResults = wrapper.PolicyResults;
+			}
+			HandlePolicyResult(retryResult, token);
+			return retryResult;
+		}
+
+		public PolicyResult<T> Handle<T>(Func<T> func, CancellationToken token = default) => throw new NotImplementedException();
+		public Task<PolicyResult> HandleAsync(Func<CancellationToken, Task> func, bool configureAwait = false, CancellationToken token = default) => throw new NotImplementedException();
+		public Task<PolicyResult<T>> HandleAsync<T>(Func<CancellationToken, Task<T>> func, bool configureAwait = false, CancellationToken token = default) => throw new NotImplementedException();
+	}
+}
---
+++ tests/SimplePolicyTests.cs
@@ -0,0 +1,74 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+
+namespace PoliNorError.Tests
+{
+	internal class SimplePolicyTests
+	{
+		[Test]
+		public void Should_Handle_Work_When_Ok()
+		{
+			var simple = new SimplePolicy();
+			void action() => Expression.Empty();
+
+			var res = simple.Handle(action);
+			Assert.IsFalse(res.IsFailed);
+			Assert.IsFalse(res.IsCanceled);
+			Assert.IsFalse(res.Errors.Any());
+			Assert.IsTrue(res.IsOk);
+		}
+
+		[Test]
+		public void Should_Execute_CanNotHandleIfTokenCanceledAlready()
+		{
+			var cancelTokenSource = new CancellationTokenSource();
+			cancelTokenSource.Cancel();
+
+			var moqProcessor = new Mock<ISimplePolicyProcessor>();
+
+			moqProcessor.Setup((t) => t.Execute(It.IsAny<Action>(), cancelTokenSource.Token));
+
+			var policy = new SimplePolicy();
+			var res = policy.Handle(() => { }, cancelTokenSource.Token);
+
+			Assert.AreEqual(true, res.IsCanceled);
+			Assert.AreEqual(false, res.IsFailed);
+			Assert.IsFalse(res.IsSuccess);
+			Assert.IsFalse(res.IsOk);
+
+			moqProcessor.Verify((t) => t.Execute(It.IsAny<Action>(), cancelTokenSource.Token), Times.Never);
+			cancelTokenSource.Dispose();
+		}
+
+		[Test]
+		public void Should_Work_For_Handle_Null_Delegate()
+		{
+			var retryPolTest = new SimplePolicy();
+			var retryResult = retryPolTest.Handle(null);
+			Assert.IsTrue(retryResult.IsFailed);
+			Assert.AreEqual(typeof(NoDelegateException), retryResult.Errors.FirstOrDefault()?.GetType());
+		}
+
+		[Test]
+		public void Should_Handle_CallWrappedPolicy_When_Wrapped_And_Not_Error()
+		{
+			var simple = new SimplePolicy();
+
+			var wrappedPolicy = new Mock<IPolicyBase>();
+
+			Action act = () => { };
+
+			wrappedPolicy.Setup(t => t.Handle(act, default)).Returns(new PolicyResult());
+			simple.WrapPolicy(wrappedPolicy.Object);
+
+			var outPolicyResult = simple.Handle(act, default);
+			Assert.AreEqual(1, outPolicyResult.WrappedPolicyResults.Count());
+
+			wrappedPolicy.Verify((t) => t.Handle(act, default), Times.AtLeastOnce);
+		}
+	}
+}
