d3b8801c0aa33f1080bde1926e427bb53703a83b
kolan72 <kolan72@gmail.com>
2023-07-01 19:03:01 +0300

    Refactor the `SyncHandlerRunnerT` class - delegate running to the `SyncHandlerRunner` class.

--- src/HandlerRunners/SyncHandlerRunnerT.cs
+++ src/HandlerRunners/SyncHandlerRunnerT.cs
@@ -6,34 +6,23 @@ namespace PoliNorError
 {
 	internal sealed class SyncHandlerRunnerT : HandlerRunnerBase, IHandlerRunnerT
 	{
-		private readonly Action<PolicyResult, object, CancellationToken> _act;
-
+		private readonly SyncHandlerRunner _syncHandlerRunnerInner;
 		public override bool SyncRun => true;
 
 		public static SyncHandlerRunnerT Create<T>(Action<PolicyResult<T>, CancellationToken> act, int num)
 		{
-			void actArg(PolicyResult pr, object _, CancellationToken ct) => act((PolicyResult<T>)pr, ct);
-			return new SyncHandlerRunnerT(actArg, num);
+			void actArg(PolicyResult pr, CancellationToken ct) => act((PolicyResult<T>)pr, ct);
+			return new SyncHandlerRunnerT(new SyncHandlerRunner(actArg, num), num);
 		}
 
-		private SyncHandlerRunnerT(Action<PolicyResult, object, CancellationToken> act, int num) : base(num)
+		private SyncHandlerRunnerT(SyncHandlerRunner syncHandlerRunner, int num) : base(num)
 		{
-			_act = act;
+			_syncHandlerRunnerInner = syncHandlerRunner;
 		}
 
 		public void Run<T>(PolicyResult<T> policyResult, CancellationToken token = default)
 		{
-			if (token.IsCancellationRequested)
-				return;
-
-			bool wasNotFailed = false;
-			if (!policyResult.IsFailed)
-				wasNotFailed = true;
-
-			_act(policyResult, policyResult.Result, token);
-
-			if (wasNotFailed && policyResult.IsFailed)
-				policyResult.FailedReason = PolicyResultFailedReason.PolicyResultHandlerFailed;
+			_syncHandlerRunnerInner.Run(policyResult, token);
 		}
 
 		public Task RunAsync<T>(PolicyResult<T> policyResult, CancellationToken token = default)
