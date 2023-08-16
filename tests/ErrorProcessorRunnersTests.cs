b2bec76d5c362352342cb8b90fc1424ea59b2f89
kolan72 <kolan72@gmail.com>
2023-08-16 12:08:06 +0300

    New ErrorProcessorRunners tests.

--- tests/ErrorProcessorRunnersTests.cs
+++ tests/ErrorProcessorRunnersTests.cs
@@ -10,7 +10,7 @@ namespace PoliNorError.Tests
 		[Test]
 		[TestCase(true)]
 		[TestCase(false)]
-		public async Task Should_Ctor_With_Not_Cancelable_Action_Always_Processed_As_NotCancelable(bool withToken)
+		public async Task Should_ErrorProcessorFromSyncRunner_Ctor_With_Not_Cancelable_Action_Always_Processed_As_NotCancelable(bool withToken)
 		{
 			int i = 0;
 			void act(Exception _, Unit __) => ++i;
@@ -45,7 +45,7 @@ namespace PoliNorError.Tests
 		}
 
 		[Test]
-		public async Task Should_Ctor_With_Cancelable_Action_Always_ProcessedAsync_As_CancelableFunc()
+		public async Task Should_ErrorProcessorFromSyncRunner_Ctor_With_Cancelable_Action_Always_Processed_As_Cancelable()
 		{
 			int i = 0;
 			void act(Exception _, Unit __) => ++i;
@@ -53,6 +53,65 @@ namespace PoliNorError.Tests
 			var cancelSource = new CancellationTokenSource();
 
 			var cancelableRunner = new ErrorProcessorFromSyncRunner<Unit>(act, CancellationType.Cancelable);
+
+			var resAsyncWithCancelTokenCancelable = await cancelableRunner.RunAsync(ex, Unit.Default, false, cancelSource.Token);
+			Assert.AreEqual(ErrorProcessorRunResul.CancelableFuncTokenExists, resAsyncWithCancelTokenCancelable);
+			Assert.AreEqual(1, i);
+
+			var resSyncWithCancelTokenCancelable = cancelableRunner.Run(ex, Unit.Default, cancelSource.Token);
+			Assert.AreEqual(ErrorProcessorRunResul.CancelableActionTokenExists, resSyncWithCancelTokenCancelable);
+			Assert.AreEqual(2, i);
+
+			cancelSource.Dispose();
+		}
+
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public async Task Should_ErrorProcessorFromAsyncRunner_Ctor_With_Not_Cancelable_Func_Always_Processed_As_NotCancelable(bool withToken)
+		{
+			int i = 0;
+			async Task act(Exception _, Unit __)  { await Task.Delay(1); ++i; }
+
+			var notCancelableRunner = new ErrorProcessorFromAsyncRunner<Unit>(act);
+			var ex = new Exception();
+
+			if (!withToken)
+			{
+				var resAsyncWithNoCancelToken = await notCancelableRunner.RunAsync(ex, Unit.Default, false);
+				Assert.AreEqual(ErrorProcessorRunResul.NotCancelableFuncNoToken, resAsyncWithNoCancelToken);
+				Assert.AreEqual(1, i);
+
+				var resSyncWithNoCancelToken = notCancelableRunner.Run(ex, Unit.Default);
+				Assert.AreEqual(ErrorProcessorRunResul.NotCancelableActionNoToken, resSyncWithNoCancelToken);
+				Assert.AreEqual(2, i);
+			}
+			else
+			{
+				var cancelSource = new CancellationTokenSource();
+
+				var resSyncWithCancelToken = notCancelableRunner.Run(ex, Unit.Default, cancelSource.Token);
+				Assert.AreEqual(ErrorProcessorRunResul.NotCancelableActionTokenExists, resSyncWithCancelToken);
+				Assert.AreEqual(1, i);
+
+				var resAsyncWithCancelToken = await notCancelableRunner.RunAsync(ex, Unit.Default, false, cancelSource.Token);
+				Assert.AreEqual(ErrorProcessorRunResul.NotCancelableFuncTokenExists, resAsyncWithCancelToken);
+				Assert.AreEqual(2, i);
+
+				cancelSource.Dispose();
+			}
+		}
+
+		[Test]
+		public async Task Should_ErrorProcessorFromAsyncRunner_Ctor_With_Cancelable_Func_Always_Processed_As_Cancelable()
+		{
+			int i = 0;
+			async Task act(Exception _, Unit __) { await Task.Delay(1); ++i; }
+
+			var ex = new Exception();
+			var cancelSource = new CancellationTokenSource();
+
+			var cancelableRunner = new ErrorProcessorFromAsyncRunner<Unit>(act, CancellationType.Cancelable);
 
 			var resAsyncWithCancelTokenCancelable = await cancelableRunner.RunAsync(ex, Unit.Default, false, cancelSource.Token);
 			Assert.AreEqual(ErrorProcessorRunResul.CancelableFuncTokenExists, resAsyncWithCancelTokenCancelable);
