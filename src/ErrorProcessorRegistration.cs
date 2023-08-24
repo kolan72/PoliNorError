c5f87598a943b9f45ef0831469c6d85bf599b83e
kolan72 <kolan72@gmail.com>
2023-08-24 14:28:33 +0300

    Split the `IPolicyProcessorExtensions` class into more specialized classes.

---
+++ src/ErrorProcessorRegistration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError
+{
+	/// <summary>
+	/// Provides a set of static methods to add error processor to policy processor.
+	/// </summary>
+	public static class ErrorProcessorRegistration
+	{
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Action<Exception> actionProcessor) where T : IPolicyProcessor
+						=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(actionProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Action<Exception, CancellationToken> actionProcessor) where T : IPolicyProcessor
+						=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(actionProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Action<Exception> actionProcessor, CancellationType cancellationType) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(actionProcessor, cancellationType));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, Task> funcProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(funcProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, Task> funcProcessor, CancellationType cancellationType) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(funcProcessor, cancellationType));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, CancellationToken, Task> funcProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(funcProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, CancellationToken, Task> funcProcessor, Action<Exception> actionProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(funcProcessor, actionProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, CancellationToken, Task> funcProcessor, Action<Exception> actionProcessor, CancellationType cancellationType) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(funcProcessor, actionProcessor, cancellationType));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, Task> funcProcessor, Action<Exception> actionProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(funcProcessor, actionProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, Task> funcProcessor, Action<Exception> actionProcessor, CancellationType cancellationType) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new BasicErrorProcessor(funcProcessor, actionProcessor, cancellationType));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Action<Exception, ProcessingErrorInfo> actionProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(actionProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Action<Exception, ProcessingErrorInfo, CancellationToken> actionProcessor) where T : IPolicyProcessor
+						=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(actionProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Action<Exception, ProcessingErrorInfo> actionProcessor, CancellationType cancellationType) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(actionProcessor, cancellationType));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, ProcessingErrorInfo, Task> funcProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(funcProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, ProcessingErrorInfo, Task> funcProcessor, CancellationType cancellationType) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(funcProcessor, cancellationType));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, ProcessingErrorInfo, CancellationToken, Task> funcProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(funcProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, ProcessingErrorInfo, CancellationToken, Task> funcProcessor, Action<Exception, ProcessingErrorInfo> actionProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(funcProcessor, actionProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, ProcessingErrorInfo, CancellationToken, Task> funcProcessor, Action<Exception, ProcessingErrorInfo> actionProcessor, CancellationType cancellationType) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(funcProcessor, actionProcessor, cancellationType));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, ProcessingErrorInfo, Task> funcProcessor, Action<Exception, ProcessingErrorInfo> actionProcessor) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(funcProcessor, actionProcessor));
+
+		public static T WithErrorProcessorOf<T>(this T policyProcessor, Func<Exception, ProcessingErrorInfo, Task> funcProcessor, Action<Exception, ProcessingErrorInfo> actionProcessor, CancellationType cancellationType) where T : IPolicyProcessor
+					=> WithErrorProcessor(policyProcessor, new DefaultErrorProcessor(funcProcessor, actionProcessor, cancellationType));
+
+		public static T WithErrorProcessor<T>(this T policyProcessor, IErrorProcessor errorProcessor) where T : IPolicyProcessor
+		{
+			if (errorProcessor == null)
+				throw new ArgumentNullException(nameof(errorProcessor));
+
+			policyProcessor.AddErrorProcessor(errorProcessor);
+			return policyProcessor;
+		}
+	}
+}
---
+++ src/PolicyProcessorFiltering.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace PoliNorError
+{
+	internal static class PolicyProcessorFiltering
+	{
+		internal static T IncludeError<T, TException>(this T policyProcessor, Func<TException, bool> func = null) where T : IPolicyProcessor where TException : Exception
+		{
+			policyProcessor.AddIncludedErrorFilter(func);
+			return policyProcessor;
+		}
+
+		internal static T IncludeError<T>(this T policyProcessor, Expression<Func<Exception, bool>> handledErrorFilter) where T : IPolicyProcessor
+		{
+			policyProcessor.AddIncludedErrorFilter(handledErrorFilter);
+			return policyProcessor;
+		}
+
+		internal static T ExcludeError<T, TException>(this T policyProcessor, Func<TException, bool> func = null) where T : IPolicyProcessor where TException : Exception
+		{
+			policyProcessor.AddExcludedErrorFilter(func);
+			return policyProcessor;
+		}
+
+		internal static T ExcludeError<T>(this T policyProcessor, Expression<Func<Exception, bool>> handledErrorFilter) where T : IPolicyProcessor
+		{
+			policyProcessor.AddExcludedErrorFilter(handledErrorFilter);
+			return policyProcessor;
+		}
+
+		internal static void AddIncludedErrorFilter(this IPolicyProcessor policyProcessor, Expression<Func<Exception, bool>> handledErrorFilter)
+		{
+			policyProcessor.ErrorFilter.AddIncludedErrorFilter(handledErrorFilter);
+		}
+
+		internal static void AddIncludedErrorFilter<TException>(this IPolicyProcessor policyProcessor, Func<TException, bool> func = null) where TException : Exception
+		{
+			policyProcessor.ErrorFilter.AddIncludedErrorFilter(ExpressionHelper.GetTypedErrorFilter(func));
+		}
+
+		internal static void AddExcludedErrorFilter(this IPolicyProcessor policyProcessor, Expression<Func<Exception, bool>> handledErrorFilter)
+		{
+			policyProcessor.ErrorFilter.AddExcludedErrorFilter(handledErrorFilter);
+		}
+
+		internal static void AddExcludedErrorFilter<TException>(this IPolicyProcessor policyProcessor, Func<TException, bool> func = null) where TException : Exception
+		{
+			policyProcessor.ErrorFilter.AddExcludedErrorFilter(ExpressionHelper.GetTypedErrorFilter(func));
+		}
+	}
+}
