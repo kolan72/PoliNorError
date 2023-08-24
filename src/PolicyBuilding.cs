1ae582936909d9a080afee0d55243891d7fc112d
kolan72 <kolan72@gmail.com>
2023-08-24 14:16:54 +0300

    Split the `HandleErrorPolicyBaseExtensions` class into more specialized classes.

---
+++ src/PolicyBuilding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PoliNorError
+{
+	/// <summary>
+	/// Provides extra methods to build policy that inherits from <see cref="Policy"></see>
+	/// </summary>
+	public static class PolicyBuilding
+	{
+		public static T WrapPolicy<T>(this T errorPolicyBase, IPolicyBase wrappedPolicy) where T : Policy
+		{
+			if (errorPolicyBase._wrappedPolicy != null)
+			{
+				throw new NotImplementedException("More than one wrapped policy is not supported.");
+			}
+			errorPolicyBase._wrappedPolicy = wrappedPolicy;
+			return errorPolicyBase;
+		}
+
+		public static T WithPolicyName<T>(this T errorPolicyBase, string policyName) where T : Policy
+		{
+			errorPolicyBase.PolicyName = policyName;
+			return errorPolicyBase;
+		}
+	}
+}
---
+++ src/PolicyResultHandlerRegistration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError
+{
+	internal static class PolicyResultHandlerRegistration
+	{
+		internal static T AddPolicyResultHandlerInner<T>(this T errorPolicyBase, Action<PolicyResult> action) where T : Policy
+		{
+			errorPolicyBase.AddSyncHandler(action);
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T>(this T errorPolicyBase, Action<PolicyResult> action, CancellationType convertType) where T : Policy
+		{
+			return errorPolicyBase.AddPolicyResultHandlerInner(action.ToCancelableAction(convertType));
+		}
+
+		internal static T AddPolicyResultHandlerInner<T>(this T errorPolicyBase, Action<PolicyResult, CancellationToken> action) where T : Policy
+		{
+			errorPolicyBase.AddSyncHandler(action);
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T>(this T errorPolicyBase, Func<PolicyResult, Task> func) where T : Policy
+		{
+			errorPolicyBase.AddAsyncHandler(func);
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T>(this T errorPolicyBase, Func<PolicyResult, Task> func, CancellationType convertType) where T : Policy
+		{
+			errorPolicyBase.AddPolicyResultHandlerInner(func.ToCancelableFunc(convertType));
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T>(this T errorPolicyBase, Func<PolicyResult, CancellationToken, Task> func) where T : Policy
+		{
+			errorPolicyBase.AddAsyncHandler(func);
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T, U>(this T errorPolicyBase, Func<PolicyResult<U>, Task> func) where T : Policy
+		{
+			errorPolicyBase.AddAsyncHandler(func);
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T, U>(this T errorPolicyBase, Func<PolicyResult<U>, Task> func, CancellationType convertType) where T : Policy
+		{
+			errorPolicyBase.AddPolicyResultHandlerInner(func.ToCancelableFunc(convertType));
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T, U>(this T errorPolicyBase, Func<PolicyResult<U>, CancellationToken, Task> func) where T : Policy
+		{
+			errorPolicyBase.AddAsyncHandler(func);
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T, U>(this T errorPolicyBase, Action<PolicyResult<U>> action) where T : Policy
+		{
+			errorPolicyBase.AddSyncHandler(action);
+			return errorPolicyBase;
+		}
+
+		internal static T AddPolicyResultHandlerInner<T, U>(this T errorPolicyBase, Action<PolicyResult<U>> action, CancellationType convertType) where T : Policy
+		{
+			return errorPolicyBase.AddPolicyResultHandlerInner(action.ToCancelableAction(convertType));
+		}
+
+		internal static T AddPolicyResultHandlerInner<T, U>(this T errorPolicyBase, Action<PolicyResult<U>, CancellationToken> action) where T : Policy
+		{
+			errorPolicyBase.AddSyncHandler(action);
+			return errorPolicyBase;
+		}
+	}
+}
