ecea1551e0621d578c471d833dcc8635564e8c48
kolan72 <kolan72@gmail.com>
2023-08-24 15:59:39 +0300

    Rename `IErrorProcessorExtensions` class to `ErrorProcessorAsyncProcessing`.

---
+++ src/ErrorProcessors/ErrorProcessorAsyncProcessing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError
+{
+	/// <summary>
+	/// Provides static helper methods to invoke IErrorProcessor.ProcessAsync method when the configureAwait parameter is set to false.
+	/// </summary>
+	public static class ErrorProcessorAsyncProcessing
+	{
+		public static Task<Exception> ProcessAsync(this IErrorProcessor errorProcessor,
+														Exception error,
+														ProcessingErrorInfo catchBlockProcessErrorInfo,
+														CancellationToken cancellationToken = default) =>
+			errorProcessor.ProcessAsync(error, catchBlockProcessErrorInfo, false, cancellationToken);
+	}
+}
