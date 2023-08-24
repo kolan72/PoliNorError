2e861d54ad2159ff323fbd66991b917e79305505
kolan72 <kolan72@gmail.com>
2023-08-24 15:55:45 +0300

    Split `IFallbackProcessorExtensions` class into more specialized classes.

---
+++ src/Fallback/FallbackProcessorAsyncExecuting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError
+{
+	/// <summary>
+	///	Provides static helper methods to invoke IFallbackProcessor.FallbackAsync methods when the configureAwait parameter is set to false.
+	/// </summary>
+	public static class FallbackProcessorAsyncExecuting
+	{
+		public static Task<PolicyResult> FallbackAsync(this IFallbackProcessor fallbackProcessor, Func<CancellationToken, Task> func, Func<CancellationToken, Task> fallback, CancellationToken token)
+																=> fallbackProcessor.FallbackAsync(func, fallback, false, token);
+
+		public static Task<PolicyResult<T>> FallbackAsync<T>(this IFallbackProcessor fallbackProcessor, Func<CancellationToken, Task<T>> func, Func<CancellationToken, Task<T>> fallback, CancellationToken token)
+															=> fallbackProcessor.FallbackAsync(func, fallback, false, token);
+	}
+}
---
+++ src/Fallback/FallbackProcessorErrorFiltering.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace PoliNorError
+{
+	/// <summary>
+	///  Provides static methods to set error filters to IFallbackProcessor.
+	/// </summary>
+	public static class FallbackProcessorErrorFiltering
+	{
+		public static IFallbackProcessor IncludeError<TException>(this IFallbackProcessor fallbackProcessor, Func<TException, bool> func = null) where TException : Exception => fallbackProcessor.IncludeError<IFallbackProcessor, TException>(func);
+
+		public static IFallbackProcessor IncludeError(this IFallbackProcessor fallbackProcessor, Expression<Func<Exception, bool>> handledErrorFilter) => fallbackProcessor.IncludeError<IFallbackProcessor>(handledErrorFilter);
+
+		public static IFallbackProcessor ExcludeError<TException>(this IFallbackProcessor fallbackProcessor, Func<TException, bool> func = null) where TException : Exception => fallbackProcessor.ExcludeError<IFallbackProcessor, TException>(func);
+
+		public static IFallbackProcessor ExcludeError(this IFallbackProcessor fallbackProcessor, Expression<Func<Exception, bool>> handledErrorFilter) => fallbackProcessor.ExcludeError<IFallbackProcessor>(handledErrorFilter);
+	}
+}
