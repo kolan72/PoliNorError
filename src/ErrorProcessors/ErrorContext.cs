9d1502dd80e258322329ff51e14339ff6471f671
kolan72 <kolan72@gmail.com>
2023-08-28 19:34:51 +0300

    Add `PolicyProcessorCatchBlockSyncHandler` abstract class.

---
+++ src/ErrorProcessors/ErrorContext.cs
@@ -0,0 +1,14 @@
+﻿namespace PoliNorError
+{
+	public abstract class ErrorContext<T>
+	{
+		protected ErrorContext(T t)
+		{
+			Context = t;
+		}
+
+		public T Context { get; }
+
+		public abstract ProcessingErrorContext ToProcessingErrorContext();
+	}
+}
