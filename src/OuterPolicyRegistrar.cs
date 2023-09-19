a18b563755dfbd800000a27adf590fee9ee3a166
kolan72 <kolan72@gmail.com>
2023-09-19 18:40:15 +0300

    Add docs for wrapping up.

--- src/OuterPolicyRegistrar.cs
+++ src/OuterPolicyRegistrar.cs
@@ -1,5 +1,9 @@
 ﻿namespace PoliNorError
 {
+	/// <summary>
+	/// Represents a registrar for a wrapper policy that wraps the other policy
+	/// </summary>
+	/// <typeparam name="TWrapperPolicy">The type of the wrapper policy</typeparam>
 	public class OuterPolicyRegistrar<TWrapperPolicy> where TWrapperPolicy : Policy
 	{
 		internal OuterPolicyRegistrar(TWrapperPolicy wrapperPolicy, IPolicyBase policyBase)
@@ -8,6 +12,9 @@
 			OuterPolicy._wrappedPolicy = policyBase;
 		}
 
+		/// <summary>
+		/// Returns a  wrapper policy that has wrapped the other policy.
+		/// </summary>
 		public TWrapperPolicy OuterPolicy { get; }
 	}
 }
