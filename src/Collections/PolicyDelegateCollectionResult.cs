7d5bc566c6e2cb129797e2faac91baad187724db
kolan72 <kolan72@gmail.com>
2023-11-13 17:38:35 +0300

    Extract `PolicyDelegateCollectionResultBase` class.

--- src/Collections/PolicyDelegateCollectionResult.cs
+++ src/Collections/PolicyDelegateCollectionResult.cs
@@ -4,15 +4,13 @@ using System.Linq;
 
 namespace PoliNorError
 {
-	public class PolicyDelegateCollectionResult : IEnumerable<PolicyDelegateResult>
+	public class PolicyDelegateCollectionResult : PolicyDelegateCollectionResultBase, IEnumerable<PolicyDelegateResult>
 	{
 		internal PolicyDelegateCollectionResult(IEnumerable<PolicyDelegateResult> policyHandledResults, IEnumerable<PolicyDelegate> policyDelegatesUnused, LastPolicyResultState lastPolicyResultState = null, PolicyResultFailedReason failedReason = PolicyResultFailedReason.None)
+			:base(policyHandledResults, lastPolicyResultState, failedReason)
 		{
 			PolicyDelegateResults = policyHandledResults;
 			PolicyDelegatesUnused = policyDelegatesUnused;
-			IsFailed = PolicyDelegateResults.GetLastResultFailed() || (lastPolicyResultState?.IsFailed == true);
-			IsSuccess = PolicyDelegateResults.GetLastResultSuccess();
-			LastPolicyResultFailedReason = PolicyDelegateResults.LastOrDefault()?.FailedReason ?? failedReason;
 		}
 
 		public IEnumerable<PolicyDelegateResult> PolicyDelegateResults { get; }
@@ -21,26 +19,18 @@ namespace PoliNorError
 
 		public PolicyResult LastPolicyResult => PolicyDelegateResults.LastOrDefault()?.Result;
 
-		public bool IsFailed { get; }
-
-		public bool IsSuccess { get; }
-
-		public PolicyResultFailedReason LastPolicyResultFailedReason { get; internal set; }
-
 		public IEnumerator<PolicyDelegateResult> GetEnumerator() => PolicyDelegateResults.GetEnumerator();
 
 		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 	}
 
-	public class PolicyDelegateCollectionResult<T> : IEnumerable<PolicyDelegateResult<T>>
+	public class PolicyDelegateCollectionResult<T> : PolicyDelegateCollectionResultBase, IEnumerable<PolicyDelegateResult<T>>
 	{
 		internal PolicyDelegateCollectionResult(IEnumerable<PolicyDelegateResult<T>> policyHandledResultsT, IEnumerable<PolicyDelegate<T>> policyDelegatesUnused, LastPolicyResultState lastPolicyResultState = null, PolicyResultFailedReason failedReason = PolicyResultFailedReason.None)
-        {
+			: base(policyHandledResultsT, lastPolicyResultState, failedReason)
+		{
 			PolicyDelegateResults = policyHandledResultsT;
 			PolicyDelegatesUnused = policyDelegatesUnused;
-			IsFailed = PolicyDelegateResults.GetLastResultFailed() || (lastPolicyResultState?.IsFailed == true);
-			IsSuccess = PolicyDelegateResults.GetLastResultSuccess();
-			LastPolicyResultFailedReason = PolicyDelegateResults.LastOrDefault()?.FailedReason ?? failedReason;
 		}
 
 		public IEnumerable<PolicyDelegateResult<T>> PolicyDelegateResults { get; }
@@ -57,14 +47,24 @@ namespace PoliNorError
 
 		public PolicyResult<T> LastPolicyResult => PolicyDelegateResults.LastOrDefault()?.Result;
 
+		public IEnumerator<PolicyDelegateResult<T>> GetEnumerator() => PolicyDelegateResults.GetEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
+
+	public class PolicyDelegateCollectionResultBase
+	{
+		internal PolicyDelegateCollectionResultBase(IEnumerable<PolicyDelegateResultBase> policyHandledResults, LastPolicyResultState lastPolicyResultState = null, PolicyResultFailedReason failedReason = PolicyResultFailedReason.None)
+		{
+			IsFailed = policyHandledResults.GetLastResultFailed() || (lastPolicyResultState?.IsFailed == true);
+			IsSuccess = policyHandledResults.GetLastResultSuccess();
+			LastPolicyResultFailedReason = policyHandledResults.LastOrDefault()?.FailedReason ?? failedReason;
+		}
+
 		public bool IsFailed { get; }
 
 		public bool IsSuccess { get; }
 
 		public PolicyResultFailedReason LastPolicyResultFailedReason { get; internal set; }
-
-		public IEnumerator<PolicyDelegateResult<T>> GetEnumerator() => PolicyDelegateResults.GetEnumerator();
-
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 	}
 }
