9b419a4da74ff296d800878a6970970b2d053d8d
kolan72 <kolan72@gmail.com>
2023-12-27 10:51:09 +0300

    Document `ISimplePolicyProcessor.WithInnerErrorProcessorOf` methods with comments.

--- tests/InnerErrorProcessorFuncs.cs
+++ tests/InnerErrorProcessorFuncs.cs
@@ -15,18 +15,24 @@ namespace PoliNorError.Tests
 
 		public void Action<TException>(TException _) => I++;
 
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
 		public void ActionWithToken<TException>(TException _, CancellationToken __) => J++;
 
 		public async Task AsyncFunc<TException>(TException _) { await Task.Delay(1); I++; }
 
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
 		public async Task AsyncFuncWithToken<TException>(TException _, CancellationToken __) { await Task.Delay(1); J++; }
 
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
 		public void ActionWithErrorInfo<TException>(TException _, ProcessingErrorInfo __) => K++;
 
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
 		public void ActionWithErrorInfoWithToken<TException>(TException _, ProcessingErrorInfo __, CancellationToken ___) => L++;
 
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
 		public async Task AsyncFuncWithErrorInfo<TException>(TException _, ProcessingErrorInfo __) { await Task.Delay(1); K++; }
 
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
 		public async Task AsyncFuncWithErrorInfoWithToken<TException>(TException _, ProcessingErrorInfo __, CancellationToken ___) { await Task.Delay(1); L++; }
 	}
 }
