c2994dc1c526ab9611382510c57540834128f50f
kolan72 <kolan72@gmail.com>
2024-02-27 16:23:05 +0300

    Introduce `CatchBlockHandler` class.

---
+++ src/CatchBlockHandlers/CatchBlockHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace PoliNorError
+{
+#pragma warning disable RCS1225 // Make class sealed.
+	public class CatchBlockHandler : ICanAddErrorProcessor
+#pragma warning restore RCS1225 // Make class sealed.
+	{
+		private readonly CatchBlockFilter _catchBlockFilter;
+
+		private CatchBlockHandler(CatchBlockFilter catchBlockFilter)
+		{
+			_catchBlockFilter = catchBlockFilter;
+		}
+
+		public static CatchBlockHandler FilterExceptionBy(CatchBlockFilter catchBlockFilter) => new CatchBlockHandler(catchBlockFilter);
+
+		public static CatchBlockHandler ForAllExceptions() => new CatchBlockHandler(new CatchBlockFilter());
+
+		internal PolicyProcessor.ExceptionFilter ErrorFilter => _catchBlockFilter.ErrorFilter;
+
+		internal IBulkErrorProcessor BulkErrorProcessor { get; } = new BulkErrorProcessor();
+	}
+}
