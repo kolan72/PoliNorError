9a9af68a84b8b6c29905ebb3587bf3dc30d91199
kolan72 <kolan72@gmail.com>
2024-03-04 10:51:14 +0300

    Add docs to `CatchBlockHandler` class.

--- src/CatchBlockHandlers/CatchBlockHandler.cs
+++ src/CatchBlockHandlers/CatchBlockHandler.cs
@@ -1,5 +1,8 @@
 ﻿namespace PoliNorError
 {
+	/// <summary>
+	/// Contains <see cref="PoliNorError.CatchBlockFilter"/> and error processors that are applied to an exception within the catch block.
+	/// </summary>
 #pragma warning disable RCS1225 // Make class sealed.
 	public class CatchBlockHandler : ICanAddErrorProcessor
 #pragma warning restore RCS1225 // Make class sealed.
@@ -9,8 +12,17 @@
 			CatchBlockFilter = catchBlockFilter;
 		}
 
+		/// <summary>
+		/// Creates <see cref="CatchBlockHandler"/> that filters an exception using <paramref name="catchBlockFilter"/>.
+		/// </summary>
+		/// <param name="catchBlockFilter"><see cref="PoliNorError.CatchBlockFilter"/></param>
+		/// <returns></returns>
 		public static CatchBlockHandler FilterExceptionBy(CatchBlockFilter catchBlockFilter) => new CatchBlockHandler(catchBlockFilter);
 
+		/// <summary>
+		/// Creates <see cref="CatchBlockHandler"/> with an empty <see cref="PoliNorError.CatchBlockFilter"/>.
+		/// </summary>
+		/// <returns></returns>
 		public static CatchBlockHandler ForAllExceptions() => new CatchBlockHandler(CatchBlockFilter.Empty());
 
 		internal CatchBlockFilter CatchBlockFilter { get; }
