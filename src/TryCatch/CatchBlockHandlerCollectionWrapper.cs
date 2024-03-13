8c331c8471a6ae71b156c8d3d2e50eb05fb4fff5
kolan72 <kolan72@gmail.com>
2024-03-13 18:51:15 +0300

    Add `CatchBlockHandlerCollectionWrapper` helper class.

---
+++ src/TryCatch/CatchBlockHandlerCollectionWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PoliNorError.TryCatch
+{
+	internal static class CatchBlockHandlerCollectionWrapper
+	{
+		internal static SimplePolicy Wrap(IEnumerable<CatchBlockHandler> catchBlockHandlers)
+		{
+			var firstHandler = catchBlockHandlers?.FirstOrDefault();
+			if (firstHandler is null)
+			{
+				throw new ArgumentNullException(nameof(catchBlockHandlers), $"{nameof(catchBlockHandlers)} can not be null!");
+			}
+			var currentPolicy = new SimplePolicy(firstHandler.CatchBlockFilter, firstHandler.BulkErrorProcessor, true);
+			foreach (var handler in catchBlockHandlers.Skip(1))
+			{
+				currentPolicy = currentPolicy.WrapUp(new SimplePolicy(handler.CatchBlockFilter, handler.BulkErrorProcessor, true)).OuterPolicy;
+			}
+			return currentPolicy;
+		}
+	}
+}
--- tests/TryCatchTests.cs
+++ tests/TryCatchTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using PoliNorError.TryCatch;
 using System;
+using System.Collections.Generic;
 
 namespace PoliNorError.Tests
 {
@@ -31,6 +32,50 @@ namespace PoliNorError.Tests
 				Assert.That(tryCatchResult.Error, Is.Null);
 				Assert.That(tryCatchResult.IsError, Is.False);
 			}
+		}
+
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public void Should_CatchBlockHandlerCollectionWrapper_Wrap_Correctly(bool handleByFirst)
+		{
+			var collection = new List<CatchBlockHandler>();
+			var errorToThrow = new InvalidOperationException();
+			int i = 0;
+			int k = 0;
+			var nofilterHandler = CatchBlockHandlerFactory
+				.ForAllExceptions()
+				.WithErrorProcessorOf((_) => i++);
+			var filteredHandler = CatchBlockHandlerFactory
+				.FilterExceptionsBy(NonEmptyCatchBlockFilter.CreateByIncluding<InvalidOperationException>())
+				.WithErrorProcessorOf((_) => k++);
+
+			if (handleByFirst)
+			{
+				collection.Add(nofilterHandler);
+				collection.Add(filteredHandler);
+			}
+			else
+			{
+				collection.Add(filteredHandler);
+				collection.Add(nofilterHandler);
+			}
+			var sp = CatchBlockHandlerCollectionWrapper.Wrap(collection);
+			var res = sp.Handle(() => throw errorToThrow);
+			var error = res.GetErrorInWrappedResults();
+
+			if (handleByFirst)
+			{
+				Assert.That(i, Is.EqualTo(1));
+				Assert.That(k, Is.EqualTo(0));
+			}
+			else
+			{
+				Assert.That(i, Is.EqualTo(0));
+				Assert.That(k, Is.EqualTo(1));
+			}
+
+			Assert.That(error, Is.EqualTo(errorToThrow));
 		}
 	}
 }
