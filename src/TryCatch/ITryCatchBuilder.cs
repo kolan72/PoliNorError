db8248843edbaf7efd07ddab5eee9558bda122c7
kolan72 <kolan72@gmail.com>
2024-03-22 15:08:09 +0300

    Add docs to `ITryCatchBuilder` interface and `TryCatchBuilder` class.

--- src/TryCatch/ITryCatchBuilder.cs
+++ src/TryCatch/ITryCatchBuilder.cs
@@ -1,7 +1,14 @@
 ﻿namespace PoliNorError.TryCatch
 {
+	/// <summary>
+	/// Defines method to build  <see cref="ITryCatch"/>.
+	/// </summary>
 	public interface ITryCatchBuilder
 	{
+		/// <summary>
+		/// Builds <see cref="ITryCatch"/>.
+		/// </summary>
+		/// <returns></returns>
 		ITryCatch Build();
 	}
 }
--- src/TryCatch/TryCatchBuilder.cs
+++ src/TryCatch/TryCatchBuilder.cs
@@ -2,6 +2,9 @@
 
 namespace PoliNorError.TryCatch
 {
+	/// <summary>
+	/// Default implementation of <see cref="ITryCatchBuilder"/>.
+	/// </summary>
 	public sealed class TryCatchBuilder : ITryCatchBuilder
 	{
 		private readonly List<CatchBlockHandler> _catchBlockHandlers;
@@ -11,24 +14,44 @@ namespace PoliNorError.TryCatch
 			_catchBlockHandlers = new List<CatchBlockHandler>();
 		}
 
+		/// <summary>
+		/// Creates a <see cref="TryCatchBuilder"/> class based on the <see cref="CatchBlockFilteredHandler"/>. Other <see cref="CatchBlockHandler"/> handlers may be added to the created object.
+		/// </summary>
+		/// <param name="filteredHandler"><see cref="CatchBlockFilteredHandler"/></param>
+		/// <returns><see cref="TryCatchBuilder"/></returns>
 		public static TryCatchBuilder CreateFrom(CatchBlockFilteredHandler filteredHandler)
 		{
 			var builder = new TryCatchBuilder();
 			return builder.AddCatchBlock(filteredHandler);
 		}
 
+		/// <summary>
+		/// Creates a <see cref="ITryCatchBuilder"/> class based on the <see cref="CatchBlockForAllHandler"/>. No other <see cref="CatchBlockHandler"/> handlers may be added to the created object.
+		/// </summary>
+		/// <param name="filteredHandler"><see cref="CatchBlockForAllHandler"/></param>
+		/// <returns><see cref="ITryCatchBuilder"/></returns>
 		public static ITryCatchBuilder CreateFrom(CatchBlockForAllHandler filteredHandler)
 		{
 			var builder = new TryCatchBuilder();
 			return builder.AddCatchBlock(filteredHandler);
 		}
 
+		/// <summary>
+		/// Adds <see cref="CatchBlockFilteredHandler"/> handler to builder. Other <see cref="CatchBlockHandler"/> handlers may be added to the created object.
+		/// </summary>
+		/// <param name="filteredHandler"><see cref="CatchBlockFilteredHandler"/></param>
+		/// <returns><see cref="TryCatchBuilder"/></returns>
 		public TryCatchBuilder AddCatchBlock(CatchBlockFilteredHandler filteredHandler)
 		{
 			_catchBlockHandlers.Add(filteredHandler);
 			return this;
 		}
 
+		/// <summary>
+		/// Adds <see cref="CatchBlockForAllHandler"/> handler to builder. No other <see cref="CatchBlockHandler"/> handlers may be added to the created object.
+		/// </summary>
+		/// <param name="filteredHandler"><see cref="CatchBlockForAllHandler"/></param>
+		/// <returns><see cref="ITryCatchBuilder"/></returns>
 		public ITryCatchBuilder AddCatchBlock(CatchBlockForAllHandler filteredHandler)
 		{
 			_catchBlockHandlers.Add(filteredHandler);
