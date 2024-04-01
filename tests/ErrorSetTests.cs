9624f0b5c6514bd56c2d713dfde50e8a0288564b
kolan72 <kolan72@gmail.com>
2024-04-01 13:52:10 +0300

    Introduce `IErrorSet` interface and `ErrorSet` class.

---
+++ tests/ErrorSetTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace PoliNorError.Tests
+{
+	public class ErrorSetTests
+	{
+		[Test]
+		[TestCase(false)]
+		[TestCase(true)]
+		public void Should_FromError_Create_Instance_With_Error_Type(bool callWithErrorWithTheSameType)
+		{
+			var errorSet = ErrorSet.FromError<ArgumentException>();
+			if (callWithErrorWithTheSameType)
+			{
+				errorSet.WithError<ArgumentException>();
+			}
+			Assert.That(errorSet.Items.Count(), Is.EqualTo(1));
+			Assert.That(errorSet.Items.FirstOrDefault().ErrorType, Is.EqualTo(typeof(ArgumentException)));
+		}
+
+		[Test]
+		public void Shpuld_WithError_Add_Error_Type()
+		{
+			var errorSet = ErrorSet.FromError<ArgumentException>();
+			errorSet.WithError<ArgumentNullException>();
+			Assert.That(errorSet.Items.Count(), Is.EqualTo(2));
+			Assert.That(errorSet.Items.Skip(1).FirstOrDefault().ErrorType, Is.EqualTo(typeof(ArgumentNullException)));
+		}
+	}
+}
