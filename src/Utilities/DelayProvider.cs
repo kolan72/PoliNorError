d6ab507420fba61b483c5e1ca7c1165b436ccf01
kolan72 <kolan72@gmail.com>
2024-07-03 20:15:25 +0300

    Add `DelayProvider` class.

---
+++ src/Utilities/DelayProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError
+{
+	internal class DelayProvider
+	{
+		public void Backoff(TimeSpan delay, CancellationToken cancellationToken = default)
+		{
+			bool waitResult = cancellationToken.WaitHandle.WaitOne(delay);
+			if (waitResult)
+			{
+				cancellationToken.ThrowIfCancellationRequested();
+			}
+		}
+
+		public async Task BackoffAsync(TimeSpan delay, bool configAwait = false, CancellationToken cancellationToken = default)
+		{
+			await Task.Delay(delay, cancellationToken).ConfigureAwait(configAwait);
+		}
+	}
+}
