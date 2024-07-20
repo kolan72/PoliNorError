5e5bbe3e575e7492b382272dcec68a1bd53822c3
kolan72 <kolan72@gmail.com>
2024-07-20 13:46:26 +0300

    Doc `LinearRetryDelay` class.

--- src/Retry/LinearRetryDelay.cs
+++ src/Retry/LinearRetryDelay.cs
@@ -2,25 +2,35 @@
 
 namespace PoliNorError
 {
+	/// <summary>
+	/// Class to get the delay value calculated linearly.
+	/// </summary>
 	public class LinearRetryDelay : RetryDelay
-    {
-        private readonly TimeSpan _baseDelay;
+	{
+		private readonly TimeSpan _baseDelay;
 
-        public LinearRetryDelay(LinearRetryDelayOptions retryDelayOptions) : this(retryDelayOptions.BaseDelay) { }
+		/// <summary>
+		/// Initializes a new instance of <see cref="LinearRetryDelay"/>.
+		/// </summary>
+		/// <param name="retryDelayOptions"><see cref="LinearRetryDelayOptions"/></param>
+		public LinearRetryDelay(LinearRetryDelayOptions retryDelayOptions) : this(retryDelayOptions.BaseDelay) { }
 
-        internal LinearRetryDelay(TimeSpan baseDelay)
+		internal LinearRetryDelay(TimeSpan baseDelay)
 		{
-            _baseDelay = baseDelay;
-        }
+			_baseDelay = baseDelay;
+		}
 
-        public override TimeSpan GetDelay(int attempt)
-        {
-            return TimeSpan.FromMilliseconds((attempt + 1) * _baseDelay.TotalMilliseconds);
-        }
-    }
+		public override TimeSpan GetDelay(int attempt)
+		{
+			return TimeSpan.FromMilliseconds((attempt + 1) * _baseDelay.TotalMilliseconds);
+		}
+	}
 
-    public class LinearRetryDelayOptions : RetryDelayOptions
-    {
-        public override RetryDelayType DelayType => RetryDelayType.Linear;
-    }
+	/// <summary>
+	/// Represents options for the <see cref="LinearRetryDelay"/>.
+	/// </summary>
+	public class LinearRetryDelayOptions : RetryDelayOptions
+	{
+		public override RetryDelayType DelayType => RetryDelayType.Linear;
+	}
 }
