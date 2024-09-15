36710567e2e92dc08e21dac8f955ddbfe1dc6e64
kolan72 <kolan72@gmail.com>
2024-09-15 13:50:27 +0300

    Add doc comments to `CatchBlockExceptionSource`.

--- src/Exceptions/CatchBlockExceptionSource.cs
+++ src/Exceptions/CatchBlockExceptionSource.cs
@@ -1,11 +1,29 @@
 ﻿namespace PoliNorError
 {
+	/// <summary>
+	/// Represents the source of the thrown CatchBlockException.
+	/// </summary>
 	public enum CatchBlockExceptionSource
 	{
+		/// <summary>
+		/// The default value.
+		/// </summary>
 		Unknown,
+		/// <summary>
+		/// Saving handling exception thrown.
+		/// </summary>
 		ErrorSaver,
+		/// <summary>
+		/// Exception thrown in error processor during processing by <see cref="IBulkErrorProcessor"/>.
+		/// </summary>
 		ErrorProcessor,
+		/// <summary>
+		/// Exception thrown when trying to apply a policy rule.
+		/// </summary>
 		PolicyRule,
+		/// <summary>
+		///  Exception thrown when trying to apply an error filter.
+		/// </summary>
 		ErrorFilter
 	}
 }
