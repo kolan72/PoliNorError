561dfc44a376445a93357c6629ef2f7917bde709
kolan72 <kolan72@gmail.com>
2025-02-25 13:13:01 +0300

    Introduced the `DefaultRetryProcessor.RetryInfiniteWithErrorContextAsync<TErrorContext>(Func<CancellationToken, Task>, TErrorContext, bool, CancellationToken)` method.

--- src/Retry/DefaultRetryProcessor.cs
+++ src/Retry/DefaultRetryProcessor.cs
@@ -180,6 +180,11 @@ namespace PoliNorError
 			return result;
 		}
 
+		public Task<PolicyResult> RetryInfiniteWithErrorContextAsync<TErrorContext>(Func<CancellationToken, Task> func, TErrorContext param, bool configureAwait = false, CancellationToken token = default)
+		{
+			return RetryWithErrorContextAsync(func, param, RetryCountInfo.Infinite(), configureAwait, token);
+		}
+
 		public Task<PolicyResult> RetryWithErrorContextAsync<TErrorContext>(Func<CancellationToken, Task> func, TErrorContext param, int retryCount, bool configureAwait = false, CancellationToken token = default)
 		{
 			return RetryWithErrorContextAsync(func, param, RetryCountInfo.Limited(retryCount), configureAwait, token);
