eb06b45e428a452be72d3ee829aa384c27179905
kolan72 <kolan72@gmail.com>
2025-04-17 16:03:33 +0300

    Introduced the `GetRetryCount()` extensions method for `ProcessingErrorInfo`.

---
+++ tests/ProcessingErrorInfoTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace PoliNorError.Tests
+{
+	public class ProcessingErrorInfoTests
+	{
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public void Should_GetRetryCount_Return_Zero_For_Not_RetryProcessingErrorInfo(bool generic)
+		{
+			if (!generic)
+			{
+				var pi = new ProcessingErrorInfo(PolicyAlias.Simple, new ProcessingErrorContext(PolicyAlias.Simple));
+				Assert.That(pi.GetRetryCount(), Is.Zero);
+			}
+			else
+			{
+				var pi = new ProcessingErrorInfo<int>(PolicyAlias.Simple, new ProcessingErrorContext<int>(PolicyAlias.Simple, 2));
+				Assert.That(pi.GetRetryCount(), Is.Zero);
+			}
+		}
+
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public void Should_GetRetryCount_Return_NonZero_For_RetryProcessingErrorInfo(bool generic)
+		{
+			if (!generic)
+			{
+				var pi = new RetryProcessingErrorInfo(1);
+				Assert.That(pi.GetRetryCount(), Is.EqualTo(1));
+			}
+			else
+			{
+				var pi = new RetryProcessingErrorInfo<int>(new RetryProcessingErrorContext<int>(1, 2));
+				Assert.That(pi.GetRetryCount(), Is.EqualTo(1));
+			}
+		}
+	}
+}
