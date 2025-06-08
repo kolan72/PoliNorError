aa276594a06eab203816274f2d58cd1993a0abba
kolan72 <kolan72@gmail.com>
2025-06-08 11:46:46 +0300

    Add an internal extension method `GetCancellationException()` for `AggregateException`.

--- src/Utilities/ExceptionExtensions.cs
+++ src/Utilities/ExceptionExtensions.cs
@@ -9,6 +9,16 @@ namespace PoliNorError
 		public static bool HasCanceledException(this AggregateException ae, CancellationToken token) => ae.Flatten().InnerExceptions
 																														.Any(ie => ie is OperationCanceledException operationCanceledException && operationCanceledException.CancellationToken.Equals(token));
 
+		public static OperationCanceledException GetCancellationException(this AggregateException aggregateException)
+		{
+			return aggregateException.Flatten()
+								.InnerExceptions
+								.OfType<OperationCanceledException>()
+								.FirstOrDefault()
+								??
+								new OperationCanceledException();
+		}
+
 		internal static bool DataContainsKeyStringWithValue<TValue>(this Exception exception, string key, TValue value)
 		{
 			return exception.Data.Contains(key) && exception.Data[key].GetType() == typeof(TValue) && ((TValue)exception.Data[key]).Equals(value);
---
+++ tests/ExceptionExtensionsTests.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliNorError.Tests
+{
+	internal class ExceptionExtensionsTests
+	{
+        [Test]
+        public void Should_Return_New_OperationCanceledException_When_Not_Found()
+        {
+            // Arrange
+            var aggEx = new AggregateException(new InvalidOperationException(), new Exception());
+
+            // Act
+            var result = aggEx.GetCancellationException();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<OperationCanceledException>());
+        }
+
+        [Test]
+        public void Should_ReturnOperationCanceledException_WhenItIsTheOnlyException()
+        {
+            // Arrange
+            var oce = new OperationCanceledException("Single OCE");
+            var aggregateException = new AggregateException(oce);
+
+            // Act
+            var result = aggregateException.GetCancellationException();
+
+            // Assert
+            Assert.That(result, Is.SameAs(oce),
+                "Expected the same OperationCanceledException instance to be returned.");
+        }
+
+        [Test]
+        public void Should_ReturnTaskCanceledException_WhenItIsPresent()
+        {
+            // Arrange
+            var tce = new TaskCanceledException("Task canceled");
+            var aggregateException = new AggregateException(tce);
+
+            // Act
+            var result = aggregateException.GetCancellationException();
+
+            // Assert
+            Assert.That(result, Is.SameAs(tce),
+                "Expected the same TaskCanceledException instance to be returned.");
+            Assert.That(result, Is.TypeOf<TaskCanceledException>(),
+                "Expected the exception type to remain TaskCanceledException.");
+        }
+
+        [Test]
+        public void Should_ReturnFirstOperationCanceledException_WhenMultipleExist()
+        {
+            // Arrange
+            var oce1 = new OperationCanceledException("First OCE");
+            var oce2 = new OperationCanceledException("Second OCE");
+            var aggregateException = new AggregateException(oce1, new InvalidOperationException("Other error"), oce2);
+
+            // Act
+            var result = aggregateException.GetCancellationException();
+
+            // Assert
+            Assert.That(result, Is.SameAs(oce1),
+                "Expected the first OperationCanceledException to be returned.");
+        }
+
+        [Test]
+        public void Should_ReturnOperationCanceledException_WhenMixedWithOtherExceptions()
+        {
+            // Arrange
+            var oce = new OperationCanceledException("OCE among others");
+            var otherEx = new ArgumentException("Argument error");
+            var aggregateException = new AggregateException(otherEx, oce);
+
+            // Act
+            var result = aggregateException.GetCancellationException();
+
+            // Assert
+            Assert.That(result, Is.SameAs(oce),
+                "Expected the OperationCanceledException to be returned among mixed exceptions.");
+        }
+    }
+}
