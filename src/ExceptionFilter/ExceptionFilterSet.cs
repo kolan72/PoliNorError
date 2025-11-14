98a5f46ec0f17cd1d09dca1d9da148e6459be92c
kolan72 <kolan72@gmail.com>
2025-11-14 14:27:55 +0300

    Refactor `PolicyProcessor.ExceptionFilter` by extracting its logic into a new internal `ExceptionFilterSet` class.

---
+++ src/ExceptionFilter/ExceptionFilterSet.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace PoliNorError
+{
+	internal class ExceptionFilterSet
+	{
+		internal List<Expression<Func<Exception, bool>>> IncludedErrorFilters { get; } = new List<Expression<Func<Exception, bool>>>();
+
+		internal List<Expression<Func<Exception, bool>>> ExcludedErrorFilters { get; } = new List<Expression<Func<Exception, bool>>>();
+
+		internal void IncludeFilter(Expression<Func<Exception, bool>> handledErrorFilter)
+		{
+			IncludedErrorFilters.Add(handledErrorFilter);
+		}
+
+		internal void ExcludeFilter(Expression<Func<Exception, bool>> handledErrorFilter)
+		{
+			ExcludedErrorFilters.Add(handledErrorFilter);
+		}
+
+		internal void AppendFilter(ExceptionFilterSet exceptionFilter)
+		{
+			foreach (var filter in exceptionFilter.IncludedErrorFilters)
+			{
+				IncludedErrorFilters.Add(filter);
+			}
+			foreach (var filter in exceptionFilter.ExcludedErrorFilters)
+			{
+				ExcludedErrorFilters.Add(filter);
+			}
+		}
+
+		internal Func<Exception, bool> CompilePredicate()
+		{
+			var (includeMode, includeExpression) = GetIncludedErrorFilterPredicateTuple();
+			var (excludeMode, excludeExpression) = GetExcludedErrorFilterPredicateTuple();
+
+			var resMode = includeMode | excludeMode;
+
+			if ((resMode & ErrorFilterModes.Include) != 0)
+			{
+				return (resMode & ErrorFilterModes.Exclude) != 0 ? includeExpression.And(excludeExpression).Compile() : includeExpression.Compile();
+			}
+			else if ((resMode & ErrorFilterModes.Exclude) != 0)
+			{
+				return excludeExpression.Compile();
+			}
+			else
+			{
+				return (_) => true;
+			}
+		}
+
+		private (ErrorFilterModes mode, Expression<Func<Exception, bool>> expression) GetIncludedErrorFilterPredicateTuple()
+		{
+			if (IncludedErrorFilters.Count == 0)
+				return (ErrorFilterModes.None, null);
+
+			return (ErrorFilterModes.Include, IncludedErrorFilters.GetOrCombined());
+		}
+
+		private (ErrorFilterModes mode, Expression<Func<Exception, bool>> expression) GetExcludedErrorFilterPredicateTuple()
+		{
+			if (ExcludedErrorFilters.Count == 0)
+				return (ErrorFilterModes.None, null);
+
+			var res = ExcludedErrorFilters.GetOrCombined().Not();
+
+			return (ErrorFilterModes.Exclude, res);
+		}
+
+		[Flags]
+		private enum ErrorFilterModes
+		{
+			None = 0,
+			Include = 1,
+			Exclude = 2,
+			IncludeExclude = Include | Exclude
+		}
+	}
+}
---
+++ src/ExceptionFilter/PolicyProcessor.ExceptionFilter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace PoliNorError
+{
+	public abstract partial class PolicyProcessor
+	{
+		public class ExceptionFilter
+		{
+			public IEnumerable<Expression<Func<Exception, bool>>> IncludedErrorFilters => FilterSet.IncludedErrorFilters;
+
+			public IEnumerable<Expression<Func<Exception, bool>>> ExcludedErrorFilters => FilterSet.ExcludedErrorFilters;
+
+			internal ExceptionFilterSet FilterSet { get; } = new ExceptionFilterSet();
+
+			internal void AddIncludedErrorFilter(Expression<Func<Exception, bool>> handledErrorFilter)
+			{
+				FilterSet.IncludeFilter(handledErrorFilter);
+			}
+
+			internal void AddIncludedErrorFilter<TException>(Func<TException, bool> func = null) where TException : Exception
+			{
+				AddIncludedErrorFilter(ExpressionHelper.GetTypedErrorFilter(func));
+			}
+
+			internal void AddIncludedInnerErrorFilter<TException>(Func<TException, bool> func = null) where TException : Exception
+			{
+				AddIncludedErrorFilter(ExpressionHelper.GetTypedInnerErrorFilter(func));
+			}
+
+			internal void AddExcludedErrorFilter(Expression<Func<Exception, bool>> handledErrorFilter)
+			{
+				FilterSet.ExcludeFilter(handledErrorFilter);
+			}
+
+			internal void AddExcludedErrorFilter<TException>(Func<TException, bool> func = null) where TException : Exception
+			{
+				AddExcludedErrorFilter(ExpressionHelper.GetTypedErrorFilter(func));
+			}
+
+			internal void AddExcludedInnerErrorFilter<TException>(Func<TException, bool> func = null) where TException : Exception
+			{
+				AddExcludedErrorFilter(ExpressionHelper.GetTypedInnerErrorFilter(func));
+			}
+
+			internal void AppendFilter(ExceptionFilter exceptionFilter)
+			{
+				FilterSet.AppendFilter(exceptionFilter.FilterSet);
+			}
+
+			internal Func<Exception, bool> GetCanHandle()
+			{
+				return FilterSet.CompilePredicate();
+			}
+		}
+	}
+}
