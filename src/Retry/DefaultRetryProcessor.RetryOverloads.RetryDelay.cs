e93449a8154ac26496d1b7637c572ed6ec9b4bc2
kolan72 <kolan72@gmail.com>
2025-12-04 12:47:47 +0300

    Eliminate closure allocation in `DefaultRetryProcessor.RetryAsync<TParam, T>(Func<TParam, CancellationToken, Task<T>>, TParam, RetryCountInfo , RetryDelay, bool, CancellationToken)`.

--- src/Retry/DefaultRetryProcessor.RetryOverloads.RetryDelay.cs
+++ src/Retry/DefaultRetryProcessor.RetryOverloads.RetryDelay.cs
@@ -117,7 +117,7 @@ namespace PoliNorError
 
 		public Task<PolicyResult<T>> RetryAsync<TParam, T>(Func<TParam, CancellationToken, Task<T>> action, TParam param, RetryCountInfo retryCountInfo, RetryDelay retryDelay, bool configureAwait = false, CancellationToken token = default)
 		{
-			return RetryWithErrorContextAsync(action.Apply(param), param, retryCountInfo, retryDelay, configureAwait, token);
+			return RetryInternalAsync(action, param, retryCountInfo, retryDelay, configureAwait, token);
 		}
 
 		public Task<PolicyResult> RetryAsync(Func<CancellationToken, Task> func, RetryCountInfo retryCountInfo, RetryDelay retryDelay, bool configureAwait = false, CancellationToken token = default)
