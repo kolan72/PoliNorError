15f657426259ab0058a3c6acda6905b3998f1fbc
kolan72 <kolan72@gmail.com>
2025-12-26 17:03:47 +0300

    Introduced the internal `Func<T, U>.ToHandledByPolicy` extension method for the pipelined function builder.

---
+++ src/FuncPipeline/PipelineFuncExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace PoliNorError
+{
+	internal static class PipelineFuncExtensions
+	{
+		internal static Func<T, CancellationToken, PipelineResult<U>> ToHandledByPolicy<T, U>(this Func<T, U> func, SimplePolicy policy)
+		{
+			return (t, ct) =>
+			{
+				var res = policy.Handle(func, t, ct);
+
+				if (ct.IsCancellationRequested)
+				{
+					return PipelineResult<U>.Failure(res, true);
+				}
+
+				if (!res.NoError || res.IsCanceled)
+				{
+					return PipelineResult<U>.Failure(res);
+				}
+				return PipelineResult<U>.Success(res);
+			};
+		}
+	}
+}
---
+++ tests/PipelineFuncExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading;
+
+namespace PoliNorError.Tests
+{
+	public class PipelineFuncExtensionsTests
+	{
+        [Test]
+        public void Should_Return_Success_When_Policy_Handles_Successfully_And_No_Cancellation()
+        {
+            // Arrange
+            Func<int, string> func = x => x.ToString();
+            var policy = new SimplePolicy();
+            var cancellationToken = CancellationToken.None;
+
+            // Act
+            var wrappedFunc = func.ToHandledByPolicy(policy);
+            var result = wrappedFunc(42, cancellationToken);
+
+            // Assert
+            Assert.That(result.IsFailed, Is.False);
+            Assert.That(result.IsCanceled, Is.False);
+            Assert.That(result.Result, Is.EqualTo("42"));
+            Assert.That(result.SucceededPolicyResult, Is.Not.Null);
+        }
+
+        [Test]
+        public void Should_Return_Failure_When_CancellationToken_Is_Canceled()
+		{
+			// Arrange
+			Func<int, string> func = x => x.ToString();
+			var policy = new SimplePolicy();
+			using (var cts = new CancellationTokenSource())
+			{
+				cts.Cancel();
+
+				// Act
+				var wrappedFunc = func.ToHandledByPolicy(policy);
+				var result = wrappedFunc(42, cts.Token);
+
+				// Assert
+				Assert.That(result.IsFailed, Is.True);
+				Assert.That(result.IsCanceled, Is.True);
+				Assert.That(result.FailedPolicyResult, Is.Not.Null);
+				Assert.That(result.SucceededPolicyResult, Is.Null);
+			}
+		}
+	}
+}
