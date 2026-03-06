c559d78945f990845630dcdd6856ceca36e065d7
kolan72 <kolan72@gmail.com>
2026-03-06 11:21:23 +0300

    Store `ServiceOperationCanceledException` in `PolicyResult.PolicyCanceledError` when cancellation is observed on linked token during synchronous wait on tasks in `DefaultFallbackProcessor.Fallback<TParam, T>`.

--- tests/DefaultFallbackProcessorTests.cs
+++ tests/DefaultFallbackProcessorTests.cs
@@ -819,5 +819,39 @@ namespace PoliNorError.Tests
 				}
 			}
 		}
+
+		[Test]
+		[TestCase(true, true)]
+		[TestCase(true, false)]
+		[TestCase(false, true)]
+		[TestCase(false, false)]
+		public void Should_Fallback_Func_WithParam_Return_Correct_PolicyResult_When_OperationCanceledException_On_Linked_Token(bool canceledOnLinkedSource, bool waitAll)
+		{
+			using (var cts = new CancellationTokenSource())
+			{
+				var processor = new DefaultFallbackProcessor();
+
+				PolicyResult pr;
+
+				if (waitAll)
+				{
+					pr = processor.Fallback(TaskWaitingDelegates.GetFuncWithParamWithTaskWaitAll(cts, canceledOnLinkedSource), 1, (_) => 1, cts.Token);
+				}
+				else
+				{
+					pr = processor.Fallback(TaskWaitingDelegates.GetFuncWithParamWithTaskWait(cts, canceledOnLinkedSource), 1, (_) => 1, cts.Token);
+				}
+				Assert.That(pr.Errors.OfType<NullReferenceException>().Count, Is.EqualTo(0));
+				Assert.That(pr.IsFailed, Is.True);
+				if (canceledOnLinkedSource)
+				{
+					Assert.That(pr.PolicyCanceledError, Is.TypeOf<ServiceOperationCanceledException>());
+				}
+				else
+				{
+					Assert.That(pr.PolicyCanceledError, Is.TypeOf<TaskCanceledException>());
+				}
+			}
+		}
 	}
 }
