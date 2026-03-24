b7251f8c2c016b9569ade704e66e99f0cfbfa8f1
kolan72 <kolan72@gmail.com>
2026-03-24 13:26:45 +0300

    Add internal `StandardJitter` class.

---
+++ src/Retry/StandardJitter.cs
@@ -0,0 +1,12 @@
+﻿namespace PoliNorError
+{
+	internal static class StandardJitter
+	{
+		internal static double AddJitter(double delayInMs)
+		{
+			var offset = (delayInMs * RetryDelayConstants.JitterFactor) / 2;
+			var randomDelay = (delayInMs * RetryDelayConstants.JitterFactor * StaticRandom.RandDouble()) - offset;
+			return delayInMs + randomDelay;
+		}
+	}
+}
