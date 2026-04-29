73b84ef0b107e554f922a57b2bec1afd40f26b98
kolan72 <kolan72@gmail.com>
2026-04-29 18:31:49 +0300

    Introduce `IPipelineFuncBuilder<TIn, TOut>` and `IPipelineWithHandlersBuilder<TIn, TMid, TOut>` interfaces.

---
+++ src/PipelineFunc/IPipelineFuncBuilder.TIn.TOut.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+
+namespace PoliNorError
+{
+	public interface IPipelineFuncBuilder<TIn, TOut>
+	{
+		IPipelineWithHandlersBuilder<TIn, TOut, TNext> AddFunc<TNext>(Func<TOut, TNext> fNext);
+		Func<TIn, CancellationToken, PipelineResult<TOut>> Build();
+	}
+}
---
+++ src/PipelineFunc/IPipelineWithHandlersBuilder.TIn.TMid.TOut.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PoliNorError
+{
+	public interface IPipelineWithHandlersBuilder<TIn, TMid, TOut> : IPipelineFuncBuilder<TIn, TOut>
+	{
+		IPipelineFuncBuilder<TIn, TOut> OnError(Action<Exception, ProcessingErrorInfo<TMid>> actionProcessor);
+		IPipelineFuncBuilder<TIn, TOut> OnError(Func<Exception, ProcessingErrorInfo<TMid>, Task> actionProcessor);
+	}
+}
