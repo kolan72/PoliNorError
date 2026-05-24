c1a5abd3ae5056a88e1ca396ea83b141b51e5f61
kolan72 <kolan72@gmail.com>
2026-05-24 18:05:31 +0300

    Remove `ErrorProcessors` collection.

--- src/PipelineFunc/IPipelineFuncStepBuilder.TIn.TMid.TOut.cs
+++ src/PipelineFunc/IPipelineFuncStepBuilder.TIn.TMid.TOut.cs
@@ -10,8 +10,6 @@ namespace PoliNorError
 	/// <typeparam name="TOut">The output type for the pipeline.</typeparam>
 	public interface IPipelineFuncStepBuilder<TIn, TMid, TOut> : IPipelineFuncBuilder<TIn, TOut>
 	{
-		IPipelineFuncBuilder<TIn, TOut> ConfigureErrorProcessors(Action<ErrorProcessors> configure);
-
 		IPipelineFuncBuilder<TIn, TOut> ConfigureErrorProcessors(Action<ContextErrorProcessors<TMid>> configure);
 	}
 }
