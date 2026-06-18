26b6af45a66e23a7c5384ac8e79ac7f0e46ba3df
kolan72 <kolan72@gmail.com>
2026-06-18 17:38:07 +0300

    Add XML docs to `SimplePolicy.WithInnerErrorProcessorOf` overloads.

--- src/Simple/SimplePolicy.WithInnerErrorProcessorOf.cs
+++ src/Simple/SimplePolicy.WithInnerErrorProcessorOf.cs
@@ -6,61 +6,137 @@ namespace PoliNorError
 {
 	public sealed partial class SimplePolicy : IWithInnerErrorProcessor<SimplePolicy>
 	{
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">A delegate for error processor.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Action<TException> actionProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(actionProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">A delegate for error processor.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Action<TException, CancellationToken> actionProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(actionProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">A delegate for error processor.</param>
+		/// <param name="cancellationType">A cancellation type.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Action<TException> actionProcessor, CancellationType cancellationType) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(actionProcessor, cancellationType);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">A delegate for error processor.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Func<TException, Task> funcProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(funcProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">A delegate for error processor.</param>
+		/// <param name="cancellationType">A cancellation type.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Func<TException, Task> funcProcessor, CancellationType cancellationType) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(funcProcessor, cancellationType);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">A delegate for error processor.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Func<TException, CancellationToken, Task> funcProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(funcProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">A delegate for error processor.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Action<TException, ProcessingErrorInfo> actionProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(actionProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">A delegate for error processor.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Action<TException, ProcessingErrorInfo, CancellationToken> actionProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(actionProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">A delegate for error processor.</param>
+		/// <param name="cancellationType">A cancellation type.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Action<TException, ProcessingErrorInfo> actionProcessor, CancellationType cancellationType) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(actionProcessor, cancellationType);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">A delegate for error processor.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Func<TException, ProcessingErrorInfo, Task> funcProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(funcProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">A delegate for error processor.</param>
+		/// <param name="cancellationType">A cancellation type.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Func<TException, ProcessingErrorInfo, Task> funcProcessor, CancellationType cancellationType) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(funcProcessor, cancellationType);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">A delegate for error processor.</param>
+		/// <returns>A Simple policy.</returns>
 		public SimplePolicy WithInnerErrorProcessorOf<TException>(Func<TException, ProcessingErrorInfo, CancellationToken, Task> funcProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<SimplePolicy, TException>(funcProcessor);
