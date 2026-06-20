8dcd1671a00072e5c7ce04110d93ade12d6f4605
kolan72 <kolan72@gmail.com>
2026-06-20 19:46:37 +0300

    Add XML docs to `RetryPolicy.WithInnerErrorProcessorOf` overloads.

--- src/Retry/RetryPolicy.WithInnerErrorProcessorOf.cs
+++ src/Retry/RetryPolicy.WithInnerErrorProcessorOf.cs
@@ -6,61 +6,137 @@ namespace PoliNorError
 {
 	public sealed partial class RetryPolicy : IWithInnerErrorProcessor<RetryPolicy>
 	{
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">An inner error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Action<TException> actionProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(actionProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">An inner error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Action<TException, CancellationToken> actionProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(actionProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">An inner error processor.</param>
+		/// <param name="cancellationType">Cancellation type for the error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Action<TException> actionProcessor, CancellationType cancellationType) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(actionProcessor, cancellationType);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">An inner error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Func<TException, Task> funcProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(funcProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">An inner error processor.</param>
+		/// <param name="cancellationType">Cancellation type for the error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Func<TException, Task> funcProcessor, CancellationType cancellationType) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(funcProcessor, cancellationType);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">An inner error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Func<TException, CancellationToken, Task> funcProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(funcProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">An inner error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Action<TException, ProcessingErrorInfo> actionProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(actionProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">An inner error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Action<TException, ProcessingErrorInfo, CancellationToken> actionProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(actionProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="actionProcessor">An inner error processor.</param>
+		/// <param name="cancellationType">Cancellation type for the error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Action<TException, ProcessingErrorInfo> actionProcessor, CancellationType cancellationType) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(actionProcessor, cancellationType);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">An inner error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Func<TException, ProcessingErrorInfo, Task> funcProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(funcProcessor);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">An inner error processor.</param>
+		/// <param name="cancellationType">Cancellation type for the error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Func<TException, ProcessingErrorInfo, Task> funcProcessor, CancellationType cancellationType) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(funcProcessor, cancellationType);
 		}
 
+		/// <summary>
+		/// Adds an error processor for handling inner exception only if it has the <typeparamref name="TException"/> type.
+		/// </summary>
+		/// <typeparam name="TException">A type of inner exception.</typeparam>
+		/// <param name="funcProcessor">An inner error processor.</param>
+		/// <returns>Current Retry policy.</returns>
 		public RetryPolicy WithInnerErrorProcessorOf<TException>(Func<TException, ProcessingErrorInfo, CancellationToken, Task> funcProcessor) where TException : Exception
 		{
 			return this.WithInnerErrorProcessorOf<RetryPolicy, TException>(funcProcessor);
