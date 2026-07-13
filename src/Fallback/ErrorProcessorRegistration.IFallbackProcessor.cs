e70207d78ae406063a9a9d878bbcddd62634fafc
kolan72 <kolan72@gmail.com>
2026-07-13 12:34:20 +0300

    Get rid of `FallbackProcessorTypedErrorProcessorRegistration`.

--- src/Fallback/ErrorProcessorRegistration.IFallbackProcessor.cs
+++ src/Fallback/ErrorProcessorRegistration.IFallbackProcessor.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace PoliNorError
 {
-	public static class FallbackProcessorTypedErrorProcessorRegistration
+	public static partial class ErrorProcessorRegistration
 	{
 		/// <summary>
 		/// Adds a typed error processor to the fallback processor.
