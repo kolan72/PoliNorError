c27fe34e800ff605a53eb0b05ffcde929b630ae7
kolan72 <kolan72@gmail.com>
2026-08-02 19:49:18 +0300

    Add internal static `CancellationAwareInvocation` with `InvokeCapturingCancel(Action, CancellationToken) -> OperationCanceledException`, marked `[MethodImpl(MethodImplOptions.AggressiveInlining)]` for EH-region inlining.

---
+++ src/Utilities/CancellationAwareInvocation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace PoliNorError
+{
+	internal static class CancellationAwareInvocation
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static OperationCanceledException InvokeCapturingCancel(Action action, CancellationToken token)
+		{
+			try
+			{
+				action();
+				return null;
+			}
+			catch (OperationCanceledException oe) when (token.IsCancellationRequested)
+			{
+				return oe;
+			}
+			catch (AggregateException ae) when (token.IsCancellationRequested)
+			{
+				return ae.GetCancellationException(token);
+			}
+		}
+
+	}
+}
