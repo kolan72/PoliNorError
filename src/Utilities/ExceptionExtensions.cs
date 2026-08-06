41ce3d80b9ecc67687d2da0c297d798ad4dcd7d2
kolan72 <kolan72@gmail.com>
2026-08-06 13:12:01 +0300

    Refactor `AggregateException.GetCancellationException` extension method with fast/slow path pattern.

--- src/Utilities/ExceptionExtensions.cs
+++ src/Utilities/ExceptionExtensions.cs
@@ -12,14 +12,39 @@ namespace PoliNorError
 		public static bool HasCanceledException(this AggregateException ae, CancellationToken token) => ae.Flatten().InnerExceptions
 																														.Any(ie => ie is OperationCanceledException operationCanceledException && operationCanceledException.CancellationToken.Equals(token));
 
+
 		public static OperationCanceledException GetCancellationException(this AggregateException aggregateException, CancellationToken token = default)
 		{
-			var resExc = aggregateException.Flatten()
-								.InnerExceptions
-								.OfType<OperationCanceledException>()
-								.FirstOrDefault(ex => ex.CancellationToken.Equals(token));
+			// Fast path: check direct inner exceptions first (most common case)
+			var innerExceptions = aggregateException.InnerExceptions;
 
-			return resExc ?? new ServiceOperationCanceledException();
+			for (int i = 0; i < innerExceptions.Count; i++)
+			{
+				if (innerExceptions[i] is OperationCanceledException oce && oce.CancellationToken.Equals(token))
+				{
+					return oce;
+				}
+			}
+
+			// Slow path: check nested aggregate exceptions (rare case)
+			// This call won't be inlined but happens infrequently
+			return GetCancellationExceptionSlow(aggregateException, token);
+		}
+
+		private static OperationCanceledException GetCancellationExceptionSlow(AggregateException aggregateException, CancellationToken token)
+		{
+			// Check if there are nested AggregateExceptions
+			var flattenedExceptions = aggregateException.Flatten().InnerExceptions;
+
+			for (int i = 0; i < flattenedExceptions.Count; i++)
+			{
+				if (flattenedExceptions[i] is OperationCanceledException oce && oce.CancellationToken.Equals(token))
+				{
+					return oce;
+				}
+			}
+
+			return new ServiceOperationCanceledException();
 		}
 
 		internal static bool DataContainsKeyStringWithValue<TValue>(this Exception exception, string key, TValue value)
