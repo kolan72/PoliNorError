bd729bb7ccaea093eb1985ae1bab1f01f9770bdb
kolan72 <kolan72@gmail.com>
2026-08-09 08:54:01 +0300

    Route `DefaultFallbackProcessor.Fallback` methods through `CancellationAwareInvocation.InvokeCapturingCancel`.

--- src/Fallback/DefaultFallbackProcessor.cs
+++ src/Fallback/DefaultFallbackProcessor.cs
@@ -45,12 +45,8 @@ namespace PoliNorError
 
 			try
 			{
-				action(param);
-				result.SetOk();
-			}
-			catch (OperationCanceledException oe) when (token.IsCancellationRequested)
-			{
-				result.SetFailedAndCanceled(oe);
+				var oe = CancellationAwareInvocation.InvokeCapturingCancel(action, param, token);
+				result.SetResultBasedOnCancellation(oe);
 			}
 			catch (AggregateException ae) when (ae.IsOperationCanceledWithRequestedToken(token))
 			{
@@ -83,12 +79,8 @@ namespace PoliNorError
 
 			try
 			{
-				action();
-				result.SetOk();
-			}
-			catch (OperationCanceledException oe) when (token.IsCancellationRequested)
-			{
-				result.SetFailedAndCanceled(oe);
+				var oe = CancellationAwareInvocation.InvokeCapturingCancel(action, token);
+				result.SetResultBasedOnCancellation(oe);
 			}
 			catch (AggregateException ae) when (ae.IsOperationCanceledWithRequestedToken(token))
 			{
@@ -137,13 +129,8 @@ namespace PoliNorError
 
 			try
 			{
-				var resAction = func(param);
-				result.SetOk();
-				result.SetResult(resAction);
-			}
-			catch (OperationCanceledException oe) when (token.IsCancellationRequested)
-			{
-				result.SetFailedAndCanceled(oe);
+				var oe = CancellationAwareInvocation.InvokeCapturingCancel(func, param, token, out T outcome);
+				result.SetResultBasedOnCancellation(oe, outcome);
 			}
 			catch (AggregateException ae) when (ae.IsOperationCanceledWithRequestedToken(token))
 			{
@@ -176,13 +163,8 @@ namespace PoliNorError
 
 			try
 			{
-				var resAction = func();
-				result.SetOk();
-				result.SetResult(resAction);
-			}
-			catch (OperationCanceledException oe) when (token.IsCancellationRequested)
-			{
-				result.SetFailedAndCanceled(oe);
+				var oe = CancellationAwareInvocation.InvokeCapturingCancel(func, token, out T outcome);
+				result.SetResultBasedOnCancellation(oe, outcome);
 			}
 			catch (AggregateException ae) when (ae.IsOperationCanceledWithRequestedToken(token))
 			{
