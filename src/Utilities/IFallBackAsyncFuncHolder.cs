2427508fe535b28b8e1fca739fd842f8f4b2be58
kolan72 <kolan72@gmail.com>
2026-09-16 11:45:17 +0300

    Deprecate `IFallBackAsyncFuncHolder` and `FallBackAsyncFuncHolder<T>`.

--- src/Utilities/IFallBackAsyncFuncHolder.cs
+++ src/Utilities/IFallBackAsyncFuncHolder.cs
@@ -4,11 +4,17 @@ using System.Threading.Tasks;
 
 namespace PoliNorError
 {
+#pragma warning disable S1133 // Deprecated code should be removed
+	[Obsolete("This interface is obsolete.")]
+#pragma warning restore S1133 // Deprecated code should be removed
 	internal interface IFallBackAsyncFuncHolder
 	{
 		Func<CancellationToken, Task<T>> GetFallbackAsyncFunc<T>();
 	}
 
+#pragma warning disable S1133 // Deprecated code should be removed
+	[Obsolete("This class is obsolete.")]
+#pragma warning restore S1133 // Deprecated code should be removed
 	internal class FallBackAsyncFuncHolder<U> : IFallBackAsyncFuncHolder
 	{
 		private readonly Func<CancellationToken, Task<U>> _func;
--- tests/UtilitiesTests.cs
+++ tests/UtilitiesTests.cs
@@ -1,8 +1,6 @@
 ﻿using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace PoliNorError.Tests
 {
@@ -23,25 +21,6 @@ namespace PoliNorError.Tests
 			var cls = new TestClass();
 			var res = BoxingSafeConverter<TestClass, TestClass>.Instance.Convert(cls);
 			ClassicAssert.AreEqual(typeof(TestClass), res.GetType());
-		}
-
-		[Test]
-		public async Task Should_FallBackAsyncFuncHolder_Work()
-		{
-			int i = 1;
-			var holder = new FallBackAsyncFuncHolder<int>(async (_) => await Task.FromResult(++i));
-			var theSameFunc = holder.GetFallbackAsyncFunc<int>();
-			var res = await theSameFunc(new CancellationToken());
-			ClassicAssert.AreEqual(2, res);
-		}
-
-		[Test]
-		public void Should_FallBackAsyncFuncHolder_BeNull_If_Types_Not_Equals()
-		{
-			int i = 1;
-			var holder = new FallBackAsyncFuncHolder<int>(async (_) => await Task.FromResult(++i));
-			var theSameFunc = holder.GetFallbackAsyncFunc<string>();
-			ClassicAssert.IsNull(theSameFunc);
 		}
 
 		[Test]
